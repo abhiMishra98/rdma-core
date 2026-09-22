abhiMishra98/rdma-core

commit 69c3e4cbbcc7fe3f9831945c66debb7fa546f87b

util: Add open_cdev
Add a function to open a char device node by major:minor. This is intended
to be robust against module auto loading races with udev.

Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

util/open_cdev.c
@@ -1,0 +1,146 @@
+/*
+ * Copyright (c) 2019, Mellanox Technologies. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *      Redistribution and use in source and binary forms, with or
+ *      without modification, are permitted provided that the following
+ *      conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/timerfd.h>
+#include <sys/inotify.h>
+#include <sys/sysmacros.h>
+#include <poll.h>
+
+#include <util/util.h>
+
+#include <config.h>
+
+static int open_cdev_internal(const char *path, dev_t cdev)
+{
+	struct stat st;
+	int fd;
+
+	fd = open(path, O_RDWR | O_CLOEXEC);
+	if (fd == -1)
+		return -1;
+	if (fstat(fd, &st) || !S_ISCHR(st.st_mode) ||
+	    (cdev != 0 && st.st_rdev != cdev)) {
+		close(fd);
+		return -1;
+	}
+	return fd;
+}
+
+/*
+ * In case the cdev was not exactly where we should be, use this more
+ * elaborate approach to find it.  This is designed to resolve a race with
+ * module autoloading where udev is concurrently creately the cdev as we are
+ * looking for it. udev has 5 seconds to create the link or we fail.
+ *
+ * Modern userspace and kernels create the /dev/infiniband/X synchronously via
+ * devtmpfs before returning from the netlink query, so they should never use
+ * this path.
+ */
+static int open_cdev_robust(const char *devname_hint, dev_t cdev)
+{
+	struct itimerspec ts = { .it_value = { .tv_sec = 5 } };
+	struct inotify_event buf[16];
+	struct pollfd fds[2];
+	char *devpath;
+	int res = -1;
+	int ifd;
+	int tfd;
+
+	/*
+	 * This assumes that udev is being used and is creating the /dev/char/
+	 * symlinks.
+	 */
+	if (asprintf(&devpath, "/dev/char/%u:%u", major(cdev), minor(cdev)) < 0)
+		return -1;
+
+	/* Use inotify to speed up the resolution time. */
+	ifd = inotify_init1(IN_CLOEXEC | IN_NONBLOCK);
+	if (ifd == -1)
+		goto err_mem;
+	if (inotify_add_watch(ifd, "/dev/char/", IN_CREATE) == -1)
+		goto err_inotify;
+
+	/* Timerfd is simpler than working with relative time outs */
+	tfd = timerfd_create(CLOCK_MONOTONIC, TFD_CLOEXEC);
+	if (tfd == -1)
+		goto err_inotify;
+	if (timerfd_settime(tfd, 0, &ts, NULL) == -1)
+		goto out_timer;
+
+	res = open_cdev_internal(devpath, cdev);
+	if (res != -1)
+		goto out_timer;
+
+	fds[0].fd = ifd;
+	fds[0].events = POLLIN;
+	fds[1].fd = tfd;
+	fds[1].events = POLLIN;
+	while (poll(fds, 2, -1) > 0) {
+		res = open_cdev_internal(devpath, cdev);
+		if (res != -1)
+			goto out_timer;
+
+		if (fds[0].revents) {
+			if (read(ifd, buf, sizeof(buf)) == -1)
+				goto out_timer;
+		}
+		if (fds[1].revents)
+			goto out_timer;
+	}
+
+out_timer:
+	close(tfd);
+err_inotify:
+	close(ifd);
+err_mem:
+	free(devpath);
+	return res;
+}
+
+int open_cdev(const char *devname_hint, dev_t cdev)
+{
+	char *devpath;
+	int fd;
+
+	if (asprintf(&devpath, RDMA_CDEV_DIR "/%s", devname_hint) < 0)
+		return -1;
+	fd = open_cdev_internal(devpath, cdev);
+	free(devpath);
+	if (fd == -1 && cdev != 0)
+		return open_cdev_robust(devname_hint, cdev);
+	return fd;
+}

buildlib/config.h.in
@@ -12,6 +12,8 @@
 // FIXME: Remove this, The cmake version hard-requires new style CLOEXEC support
 #define STREAM_CLOEXEC "e"
 
+#define RDMA_CDEV_DIR "/dev/infiniband"
+
 #define IBV_CONFIG_DIR "@CONFIG_DIR@"
 #define RS_CONF_DIR "@CMAKE_INSTALL_FULL_SYSCONFDIR@/rdma/rsocket"
 #define IWPM_CONFIG_FILE "@CMAKE_INSTALL_FULL_SYSCONFDIR@/iwpmd.conf"

libibumad/umad.c
@@ -618,7 +618,7 @@ int umad_get_issm_path(const char *ca_name, int portnum, char path[], int max)
 	if ((umad_id = dev_to_umad_id(ca_name, portnum)) < 0)
 		return -EINVAL;
 
-	snprintf(path, max, "%s/issm%u", UMAD_DEV_DIR, umad_id);
+	snprintf(path, max, "%s/issm%u", RDMA_CDEV_DIR, umad_id);
 
 	return 0;
 }
@@ -643,7 +643,7 @@ int umad_open_port(const char *ca_name, int portnum)
 		return -EINVAL;
 
 	snprintf(dev_file, sizeof(dev_file), "%s/umad%d",
-		 UMAD_DEV_DIR, umad_id);
+		 RDMA_CDEV_DIR, umad_id);
 
 	if ((fd = open(dev_file, O_RDWR | O_NONBLOCK)) < 0) {
 		DEBUG("open %s failed: %s", dev_file, strerror(errno));

libibumad/umad.h
@@ -118,8 +118,6 @@ typedef struct ib_user_mad {
 
 #define UMAD_MAX_PORTS		64
 
-#define UMAD_DEV_DIR		"/dev/infiniband"
-
 #define SYS_CA_PORTS_DIR	"ports"
 
 #define SYS_NODE_TYPE		"node_type"

libibverbs/ibverbs.h
@@ -44,8 +44,6 @@
 #define PFX		"libibverbs: "
 #define VERBS_OPS_NUM (sizeof(struct verbs_context_ops) / sizeof(void *))
 
-#define RDMA_CDEV_DIR "/dev/infiniband"
-
 struct ibv_abi_compat_v2 {
 	struct ibv_comp_channel	channel;
 	pthread_mutex_t		in_use;

librdmacm/cma.c
@@ -369,7 +369,7 @@ struct rdma_event_channel *rdma_create_event_channel(void)
 	if (!channel)
 		return NULL;
 
-	channel->fd = open("/dev/infiniband/rdma_cm", O_RDWR | O_CLOEXEC);
+	channel->fd = open(RDMA_CDEV_DIR "/rdma_cm", O_RDWR | O_CLOEXEC);
 	if (channel->fd < 0) {
 		goto err;
 	}

util/CMakeLists.txt
@@ -10,6 +10,7 @@ publish_internal_headers(util
 set(C_FILES
   cl_map.c
   node_name_map.c
+  open_cdev.c
   rdma_nl.c
   util.c
   )

util/util.h
@@ -35,4 +35,6 @@ static inline uint64_t roundup_pow_of_two(uint64_t n)
 
 int set_fd_nonblock(int fd, bool nonblock);
 
+int open_cdev(const char *devname_hint, dev_t cdev);
+
 #endif

commit 300057e075a2c4f739f5abd19b4d0212dfff9efd

util: Keep the original errno when open_cdev() falls back
When opening /dev/infiniband/<name> fails, open_cdev() falls back to
open_cdev_robust(), which waits for /dev/char/<major>:<minor>. If the
fallback also fails, callers such as ibv_open_device() and
rdma_create_event_channel() see the fallback's errno instead of the
original one.

This is misleading inside containers, where /dev/char/ is usually
absent. A device node that exists but may not be opened (EACCES, or
EPERM from a device cgroup) is then reported as ENOENT, which points
at a missing device instead of a permission problem.

Keep the fallback as is, and restore the errno of the first attempt
when the fallback also fails.

Fixes: 69c3e4cbbcc7 ("util: Add open_cdev")
Signed-off-by: Abhishek Mishra <abhimishra.cetb@gmail.com>

util/open_cdev.c
@@ -30,6 +30,7 @@
  * SOFTWARE.
  */
 #define _GNU_SOURCE
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -134,13 +135,22 @@ err_mem:
 int open_cdev(const char *devname_hint, dev_t cdev)
 {
 	char *devpath;
-	int fd;
+	int fd, err;
 
 	if (asprintf(&devpath, RDMA_CDEV_DIR "/%s", devname_hint) < 0)
 		return -1;
+	errno = 0;
 	fd = open_cdev_internal(devpath, cdev);
+	err = errno;
 	free(devpath);
-	if (fd == -1 && cdev != 0)
-		return open_cdev_robust(devname_hint, cdev);
+	if (fd == -1 && cdev != 0) {
+		fd = open_cdev_robust(devname_hint, cdev);
+		/*
+		 * Report why the node could not be opened, not why the
+		 * fallback failed.
+		 */
+		if (fd == -1 && err)
+			errno = err;
+	}
 	return fd;
 }